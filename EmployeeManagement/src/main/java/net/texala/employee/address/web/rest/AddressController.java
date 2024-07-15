b79975d6c0678501802432d677a78c645d9af8a0
admin <admin@host.docker.internal>
2024-07-15 10:13:51 +0530

    Added Spring security Functionality

--- EmployeeManagement/src/main/java/net/texala/employee/address/web/rest/AddressController.java
+++ EmployeeManagement/src/main/java/net/texala/employee/address/web/rest/AddressController.java
@@ -93,4 +93,8 @@ public class AddressController {
 		headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"address-data.csv\"");
 		return ResponseEntity.ok().headers(headers).contentType(MediaType.parseMediaType("text/csv")).body(resource);
 	}
+	 @GetMapping("/add")
+	    public String addressHome() {
+	        return "Address home page";
+	    }
 }
